794dc53616176209ebd7e0c531e6cbbd3e035429
NixxeN001 <nixxenkikki@gmail.com>
2022-12-15 19:21:37 +0200

    Added in JSON functions
    Added in JSON functionality.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Sorting_AlgorithmsNew;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+//Console.WriteLine("Hello, World!");
+List<TestClass> testClasses = new List<TestClass>();
+
+testClasses.Add(new TestClass());
+testClasses.Add(new TestClass("Sword", 5, 2, 0.4f));
+testClasses.Add(new TestClass("Axe", 100, 3, 4f));
+testClasses.Add(new TestClass("Spear", 45, 20, 3.5f));
+
+//testClasses.ForEach(testClass => Console.WriteLine(testClass.Name));
+Console.WriteLine("Unsorted List");
+string filepath = "JSON_Test.json";
+
+testClasses.Sort();
+var namecol = new TestClass[testClasses.Count];
+testClasses.CopyTo(namecol, 0);
+
+testClasses.Sort(new PriceComaperer());
+var pricecol = new TestClass[testClasses.Count];
+testClasses.CopyTo(pricecol, 0);
+pricecol = namecol.Concat(pricecol).ToArray();
+
+testClasses.Sort(new DamageComparer());
+var damcol = new TestClass[testClasses.Count];
+testClasses.CopyTo(damcol, 0);
+damcol= damcol.Concat(pricecol).ToArray();
+
+
+testClasses.Sort(new RangeComparer());
+var finalcol = new TestClass[testClasses.Count];
+testClasses.CopyTo(finalcol, 0);
+finalcol = finalcol.Concat(damcol).ToArray();
+
+//JSON serialize
+string JsonSerial = JsonSerializer.Serialize(finalcol);
+//Console.WriteLine(Jsonstring);
+
+Console.WriteLine(JsonSerial);
+File.WriteAllText(filepath, JsonSerial);
+
+
+
+
+/*
+Console.WriteLine("\n");
+testClasses.Sort(new PriceComaperer());
+Console.WriteLine("Sorted by Price");
+testClasses.ForEach(Console.WriteLine);
+
+Console.WriteLine("\n");
+testClasses.Sort(new DamageComparer());
+Console.WriteLine("Sorted by Damage");
+testClasses.ForEach(Console.WriteLine);
+
+Console.WriteLine("\n");
+testClasses.Sort(new RangeComparer());
+Console.WriteLine("Sorted by Range");
+testClasses.ForEach(Console.WriteLine);*/
+
+
---
+++ TestClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_AlgorithmsNew
+{
+    internal class TestClass : IComparable<TestClass>
+    {
+
+        private string name = "";
+        private int price;
+        private int damage;
+        private float range;
+
+        public string Name { get => name; set => name = value; }
+        public int Price { get => price; set => price = value; }
+        public int Damage { get => damage; set => damage = value; }
+        public float Range { get => range; set => range = value; }
+
+        public TestClass()
+        {
+            Name = "Test";
+            Price = 0;
+            Damage = 0;
+            Range = 0.0f;
+
+        }
+        public TestClass(string name_, int price_, int damage_, float range_)
+        {
+            Name = name_;
+            Price = price_;
+            Damage = damage_;
+            Range = range_;
+        }
+
+        public int CompareTo(TestClass? other)
+        {
+
+            if (other != null) return Name.CompareTo(other.Name);
+            else return 1;
+        }
+
+        public override string ToString()
+        {
+            return Name + ":" + Price + ":" + Damage + ":" + Range;
+        }
+    }
+
+    class PriceComaperer : IComparer<TestClass>
+    {
+        public int Compare(TestClass? x, TestClass? y)
+        {
+            if (x != null && y != null) return x.Price - y.Price;
+            else return -1;
+        }
+    }
+    class DamageComparer : IComparer<TestClass>
+    {
+        public int Compare(TestClass? x, TestClass? y)
+        {
+            if (x != null && y != null) return x.Damage - y.Damage;
+            else return -1;
+        }
+    }
+    class RangeComparer: IComparer<TestClass>
+    {
+        public int Compare(TestClass? x, TestClass? y)
+        {
+            if (x != null && y != null) return (int)(x.Range - y.Range);
+            else return -1;
+        }
+    }
+}
