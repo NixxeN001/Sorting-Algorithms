d4dbc9a202e13e9f7086a88260f5f63ee8e37fb6
NixxeN001 <nixxenkikki@gmail.com>
2023-02-02 15:34:48 +0200

    creating an algorithm for bubble sorting.

---
+++ Class/Sorter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_AlgorithmsNew
+{
+    public class Sorter<T>
+    {
+        Sorter<T> data;
+
+
+
+
+        public void Sort(Sorter<T> data)
+        {
+
+        }
+
+        public void Sortby()
+        {
+
+        }
+    }
+}
--- Class/TestClass.cs
+++ Class/TestClass.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sorting_AlgorithmsNew.Class
+namespace Sorting_AlgorithmsNew
 {
     internal class TestClass : IComparable<TestClass>
     {
